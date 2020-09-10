4072374e89f55a9d0c118f338864dba4b374dbd1
blen_meku <mekuriablen@gmail.com>
2020-09-10 01:58:49 -0700

    define a menu method

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,42 +8,58 @@ class Cli
 
     def call
         puts "Welcome to Geology database for rocks!"
-        puts"Please choose your rock type"
+        puts"Please select a number between 1-3 to get rock type"
         Scraper.get_rock_type
-        list_rocks 
-        puts "Here is a brief decription about the rock you chose"
-        list_rocks_decription 
-        puts"Here is some example"
-        list_rocks_example
-        puts"Here is definition"
-        list_rock_definition
+        list_rocks_type 
+        # puts "Here is a brief decription about the rock you chose"
+        # list_rocks_decription 
+        # puts"Here is some example"
+        # list_rocks_example
+        # puts"Here is definition"
+        # list_rocks_property 
         
     end 
     
-    def list_rocks 
+    def list_rocks_type
         Rock.all.each.with_index(1) do | rock, i |
             puts "#{i}. #{rock.rock_name}"
         end
     end
 
 
-        def list_rocks_decription  
+        def display_rocks_decription  
         Rock.all.each.with_index(1) do | rock, i |
             puts "#{i}. #{rock.rock_decription}"
         end
     end
 
-   def list_rocks_example
+   def display_rocks_example
         Rock.all.each.with_index(1) do | rock, i |
             puts "#{i}. #{rock.rock_example}"
         end
      end
 
-    def list_rock_definition
+    def display_rocks_property 
         Rock.all.each.with_index(1) do | rock, i |
-            puts "#{i}. #{rock.rock_definition}"
+            puts "#{i}. #{rock.rock_property }"
         end
      end
+
+    def menu
+        puts "Please select a number between 1-3 to get the details."
+        input = gets.chomp 
+        
+        if !input.to_i.between?(1, Rock.all.count)
+                puts "Please select a valid number!"
+                list_rock_type
+                menu
+        else
+            rock = Rock.all[input.to_i-1]
+            list_rocks_decription(rock)
+            # list_rocks_example
+            # list_rocks_property
+            end 
+        end 
 end 
 
 
--- lib/rock.rb
+++ lib/rock.rb
@@ -1,13 +1,13 @@
 class Rock
   @@all = []
 
-    attr_accessor :rock_name, :rock_decription, :rock_example, :rock_definition
+    attr_accessor :rock_name, :rock_decription, :rock_example, :rock_property 
 
-    def initialize(rock_name,rock_decription,rock_example,rock_definition)
+    def initialize(rock_name,rock_decription,rock_example,rock_property )
         @rock_name = rock_name
         @rock_decription = rock_decription 
         @rock_example = rock_example
-        @rock_definition = rock_definition
+        @rock_property = rock_property 
         @@all << self
     end
 

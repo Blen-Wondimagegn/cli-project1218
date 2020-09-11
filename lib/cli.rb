7ba0bb2445309ff3d1bb704feb0373641ca76fb7
blen_meku <mekuriablen@gmail.com>
2020-09-11 15:13:28 -0700

    modifed rock_exmaple variable

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,56 +9,67 @@ class Cli
       #ask user to choose if they would like example (y/n) if yes display if no exit 
 
 
-    def call
+   def call
         puts "**************************************"
         puts "Welcome to Geology Database For Rocks!"
         puts "**************************************"
         puts ""  
         puts "Please pick a rock type for more details :"
         Scraper.get_rock_type
-        list_rock_type 
+        # list_rock_type 
+        # display_rock_decription
+        # display_rock_example
         # display_rock_property
-        menu 
-    end 
-    def list_rock_type
+        #  menu 
+   end 
+   def list_rock_type
         Rock.all.each.with_index(1) do | rock, i |
-            puts "#{i}. #{rock.rock_name}"
+         puts "#{i}:) #{rock.rock_name}"
         end
-    end
-
-    def display_rock_decription
+   end
+   def display_rock_decription
         Rock.all.each.with_index(1) do | rock, i |
-            puts "#{i}. #{rock.rock_decription}"
+            puts "#{i}:) #{rock.rock_decription}"
         end
-     end
+   end
 
    def display_rock_example
         Rock.all.each.with_index(1) do | rock, i |
-            puts "#{i}. #{rock.rock_example}"
+            puts "#{i}:) #{rock.rock_example}"
         end
-     end
-    
-
-    def menu
-        input = gets.chomp
-        if input.to_i.between?(1, Rock.all.count)
+   end    
+   def display_rock_property
+        Rock.all.each.with_index(1) do | rock, i |
+            puts "#{i}:) #{rock.rock_property}"
+        end
+   end
+   def menu
+         input = gets.chomp  
+         if input.to_i.between?(1, Rock.all.count)    
+        #    puts "You have chosen --> this rock type <-- 
             puts "What information would you like to find out:" 
             puts "Enter 1 for breif description"
             puts "Enter 2 for examples"
-            puts "Enter 3 for properties"   
-            input = gets.to_i 
-            if input == 1 
-            rock = Rock.all[input.to_i-1]
-            display_rock_decription
-            elsif input == 2
-            rock = Rock.all[input.to_i-1]
-            display_rock_example
-            else input == 3
-            rock = Rock.all[input.to_i-1]
-            display_rock_property
-            end 
+            puts "Enter 3 for properties" 
+            puts "Enter 0 to exit"   
+                input = gets.chomp.to_i 
+                if input == 1 
+                rock = Rock.all[input.to_i-1]
+                puts "Here is some description about the rock you picked:"
+                display_rock_decription
+                elsif input == 2
+                rock = Rock.all[input.to_i-1]
+                puts "Here is some examples for the rock you picked:"
+                display_rock_example
+                elsif input == 3
+                rock = Rock.all[input.to_i-1]
+                puts "Here is some uniq property about the rock you picked:"
+                display_rock_property
+                else
+                    exit 
+                end  
         else !input.to_i.between?(1, Rock.all.count) 
-            puts "Rock type not found, Please pick a rock type for more details" 
+            puts "Rock type not found, Please pick a rock type for more details"
             list_rock_type
             menu   
       end 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,25 +3,24 @@ class Scraper
 
 ROCK_URL= "https://www.amnh.org/exhibitions/permanent/planet-earth/how-do-we-read-the-rocks/three-types"
 
-def self.get_rock_type
+  def self.get_rock_type
 
-    page =  Nokogiri::HTML(open(ROCK_URL))
-    results = page.css("span.amnh-tile__content")
-    # puts results
-    # puts results.count
-    results.each do |rock|
-    rock_name = rock.css("span.amnh-tile__title").text.strip
-    rock_decription = rock.css("span.amnh-tile__description").text.strip
-    r_url ="https://www.amnh.org/exhibitions/permanent/planet-earth/how-do-we-read-the-rocks/three-types/"+ rock_name.split(' ')[0] 
-    r_page =  Nokogiri::HTML(open(r_url))
-    rock_example = r_page.css("h2.pt-2").text.strip
-    rock_property = r_page.css(".amnh-content p").text.strip
-    
-    Rock.new(rock_name,rock_decription,rock_example,rock_property)
+      page =  Nokogiri::HTML(open(ROCK_URL))
+      results = page.css("span.amnh-tile__content")
+      results.each do |rock|
+      rock_name = rock.css("span.amnh-tile__title").text.strip
+      #  rock_n = rock_name.split 
+      #  puts rock_n
+      # rock_n.each do |r|
+      #   puts r
+      # end
+      rock_decription = rock.css("span.amnh-tile__description").text.strip
+      r_url ="https://www.amnh.org/exhibitions/permanent/planet-earth/how-do-we-read-the-rocks/three-types/"+ rock_name.split(' ')[0] 
+      r_page =  Nokogiri::HTML(open(r_url))
+      rock_example = r_page.css("h2.pt-2").text.gsub("\n                            "," ").gsub("\n                     ", " ").gsub(" "," ").split(' ').join(', ')
+      rock_property = r_page.css(".amnh-content p").text
+      # binding.pry 
+      Rock.new(rock_name,rock_decription,rock_example,rock_property)
+        end
       end
-    end  
-  
-    
-  
-
 end 

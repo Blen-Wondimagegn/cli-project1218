ca84d04c1c8a167fefcebd23bf9e72f959bfa2e0
blen_meku <mekuriablen@gmail.com>
2020-09-10 18:10:05 -0700

    added nested loop to the menu method

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,7 +16,7 @@ class Cli
         puts ""  
         puts "Please pick a rock type for more details :"
         Scraper.get_rock_type
-        # list_rock_type 
+        list_rock_type 
         
         
         menu 
@@ -24,9 +24,16 @@ class Cli
     
     def list_rock_type
         Rock.all.each.with_index(1) do | rock, i |
-            puts "#{i}. #{rock.rock_name} : #{rock.rock_decription}"
+            puts "#{i}. #{rock.rock_name}"
         end
     end
+
+
+    def display_rock_decription
+        Rock.all.each.with_index(1) do | rock, i |
+            puts "#{i}. #{rock.rock_decription}"
+        end
+     end
 
    def display_rock_example
         Rock.all.each.with_index(1) do | rock, i |
@@ -42,32 +49,40 @@ class Cli
 
 
     def menu
-     input = nil 
-     puts "Would you like to see more detalis (y/n):"
-     user_input = gets.strip.downcase
-     if user_input == "y"
-        input = 0
-        while input != "exit"  
-        input = gets.chomp_to_i
-        #    puts "Invalid input, Please pick a number for a rock propertiy"
-        list_rock_type 
-        menu 
-            case input 
-            when 1 
-            puts "1"
-            when 2
-            puts "2"
-            when 3 
-            puts "3"
-            else
-            "error"
-            end
-        end  
-    else user_input == "n"
+     
+        
+    #  puts "Would you like to see more detalis (y/n):"
+     input = gets.chomp
+    while input != "exit" || !input.to_i.between?(1, Character.all.count) do 
+      
+         if input == 1
+              puts "Would you like to see more detalis (y/n):"
+                input = gets.strip.downcase
+                until (input == "n") do
+                    puts "h,mmmm"
+                end 
+        
+        # input = 0
+        # while input != "exit"  
+        # input = gets.chomp_to_i
+        # #    puts "Invalid input, Please pick a number for a rock propertiy"
+        # list_rock_type 
+        # menu 
+        #     case input 
+        #     when 1 
+        #     puts "1"
+        #     when 2
+        #     puts "2"
+        #     when 3 
+        #     puts "3"
+        #     else
+        #     "error"
+        #     end
+        # end  
+    else input == 2 
         puts"exiting"  
-    end 
-
-
+      end 
+   end 
  end 
 end 
 
